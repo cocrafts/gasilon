3540d307cba1b4f8719c0a0ad3926a3746a24102
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-12 20:25:14 +0800

    ignore node_modules lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  root: true,
-  extends: ['@walless/eslint-config'],
-  env: {
-    node: true,
-  },
-  globals: {
-    window: true,
-    document: true,
-    navigator: true,
-    fetch: true,
-    WebAssembly: true,
-  },
+	root: true,
+	extends: ['@walless/eslint-config'],
+	ignorePatterns: ['node_modules'],
+	env: {
+		node: true,
+	},
+	globals: {
+		window: true,
+		document: true,
+		navigator: true,
+		fetch: true,
+		WebAssembly: true,
+	},
 };
