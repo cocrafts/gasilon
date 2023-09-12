a08e14cb4f4da15b81e918f9655f17e97c99c005
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-12 19:54:52 +0800

    resgister routes for api

---
+++ api/routes/sponsor.ts
@@ -0,0 +1,5 @@
+import type { Handler } from 'express';
+
+export const requestSponsorSignature: Handler = (req, res) => {
+	res.json({ message: 'hmm' });
+};
--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
 import type { Express } from 'express';
 
+import { requestSponsorSignature } from './api/routes';
+
 export const configure = async (express: FunctionConstructor) => {
 	const app = express() as Express;
 
 	app.get('/', (_, res) => {
 		res.send('Hello World!!');
 	});
+
+	app.get('/request-sponsor-signature', requestSponsorSignature);
 
 	return app;
 };
