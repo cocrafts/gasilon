623336fd6279fd17521f4a30046c77a7ff553f09
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-09-12 20:25:36 +0800

    use local version of express instead of cli's version

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
-import type { Express } from 'express';
+import express, { json } from 'express';
 
 import { requestSponsorSignature } from './api/routes';
 
-export const configure = async (express: FunctionConstructor) => {
-	const app = express() as Express;
+export const configure = async () => {
+	const app = express();
+
+	app.use(json());
 
 	app.get('/', (_, res) => {
-		res.send('Hello World!!');
+		res.send('Hello World');
 	});
 
 	app.get('/request-sponsor-signature', requestSponsorSignature);
