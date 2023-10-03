0d4083df62cf1daefac9f3c54e7929db08764735
Tan Le <tanlethanh8266@gmail.com>
2023-10-03 13:35:34 +0700

    sync

--- packages/app/src/config/index.ts
+++ packages/app/src/config/index.ts
@@ -10,7 +10,7 @@ export const connection = new Connection(
 );
 
 export const ENV_SECRET_KEYPAIR = Keypair.fromSecretKey(
-	base58.decode(process.env.SOLANA_SECRET_KEY || ''),
+	base58.decode(process.env.SOLANA_SECRET_KEY as string),
 );
 
 export const cache = cacheManager.caching('memory', {
