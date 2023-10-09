29f40ab24b9be14b56df36c50b27efb4d51188cc
Tan Le <tanlethanh8266@gmail.com>
2023-10-09 15:33:25 +0700

    connect to redis in lambda

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,4 +1,14 @@
-import { app } from '@gasilon/app';
+import { app, getExchangeRate, redisClient } from '@gasilon/app';
 import serverlessExpress from '@vendia/serverless-express';
+
+redisClient.connect().then(async () => {
+	const rate = await getExchangeRate({
+		network: 'solana',
+		from: 'So11111111111111111111111111111111111111112',
+		to: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
+	});
+
+	console.log(rate, '<-- rate');
+});
 
 export const handler = serverlessExpress({ app });
